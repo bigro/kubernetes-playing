6295d3f5bdd049a96d3f03585e0083dd07e132ce
bigro <kitkat.jaja@gmail.com>
2019-12-04 17:24:06 +0900

    Secretを使って環境変数からユーザーとパスワードを取得する

--- src/main/java/com/example/kubernetesplaying/HelloTask.java
+++ src/main/java/com/example/kubernetesplaying/HelloTask.java
@@ -1,12 +1,21 @@
 package com.example.kubernetesplaying;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloTask implements CommandLineRunner {
+    @Value("${hello.user.name}")
+    String name;
+
+    @Value("${hello.user.password}")
+    String password;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Hello!! World!!");
+        System.out.println("User:" + name);
+        System.out.println("Password:" + password);
     }
 }
